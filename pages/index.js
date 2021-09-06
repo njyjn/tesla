dc6b6352999b486944e7af458d343208cb90cf01
Justin Ng <justin@aletheon.co>
2021-09-05 21:59:53 -0700

    Correct use of class to className (missed one)

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Vehicle from './vehicle';
 
 export default function Home() {
   return (
-    <div class='container-fluid'>
+    <div className='container-fluid'>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Hello Panda</title>
