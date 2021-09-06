d6bc0496b5c8ae85c928cf31d1e0e798680f3c29
Justin Ng <justin@aletheon.co>
2021-09-06 09:49:07 -0700

    Refactor for JSX

---
+++ src/components/charging.jsx
@@ -0,0 +1,57 @@
+import { Card, Col, ProgressBar, Row } from 'react-bootstrap';
+import Countdown from 'react-countdown';
+
+export default function Charging ({ chargeState }) {
+  const timeToFullChargeMs = chargeState.time_to_full_charge * 60 * 60 * 1000;  
+	return (
+		<Row>
+			<Col className='lg-12'>
+				<Card border='success' style={{ width: '100%' }}>
+          <Card.Body>
+            <Card.Title>⚡️ Charge Progress</Card.Title>
+              <Row className='p-3'>
+                <Col>
+                  <h1 className='display-1 text-center'>{chargeState.battery_level}%</h1>
+                  <p className='text-center'>charged</p>
+                </Col>
+                <Col>
+                  <h1 className='display-1 text-center'>
+                    <Countdown 
+                      date={Date.now() + timeToFullChargeMs}
+                      renderer={({ hours, minutes, seconds, completed }) => {
+                        if (completed) return '✅ Done';
+                        return `${hours}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+                      }}
+                    /></h1>
+                  <p className='text-center'>remaining</p>
+                </Col>
+              </Row>
+              <Row>
+                <Col className='pb-5'>
+                  <ProgressBar>
+                    <ProgressBar variant='success' now={chargeState.battery_level} key={1} />
+                    <ProgressBar animated variant='success' now={chargeState.charge_limit_soc - chargeState.battery_level} label={`charging to ${chargeState.charge_limit_soc}%`} key={2} />
+                    <ProgressBar variant='danger' now={chargeState.charge_limit_soc_max - chargeState.charge_limit_soc} key={3} />
+                  </ProgressBar>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <p className='text-center'>Charger Power</p>
+                  <h3 className='text-center'>{chargeState.charger_power}kW</h3>
+                </Col>
+                <Col>
+                  <p className='text-center'>Charger Voltage</p>
+                  <h3 className='text-center'>{chargeState.charger_voltage}V</h3>
+                </Col>
+                <Col>
+                  <p className='text-center'>Charge Rate</p>
+                  <h3 className='text-center'>{chargeState.charge_rate}kWh</h3>
+                </Col>
+              </Row>
+            </Card.Body>
+				</Card>
+			</Col>
+		</Row>
+	)
+}
---
+++ src/components/vehicle.jsx
@@ -0,0 +1,107 @@
+import useVehicle from '../useVehicle';
+import styles from '../../styles/Home.module.css';
+import Image from 'next/image';
+import Charging from './charging';
+import { Card, Col, Container, Row, Spinner } from 'react-bootstrap';
+
+const stateIconMap = {
+	'asleep': '🟡',
+	'online': '🟢',
+	'default': '🔴'
+}
+
+export default function Vehicle () {
+	const { vehicle, isLoading, isError } = useVehicle();
+	if (isLoading) return (
+    <span>
+      <Spinner animation="grow" variant='danger'/>
+      <p>Initializing...</p>
+    </span>
+  );
+	if (isError) return (
+		<span>
+      <Spinner animation="grow" variant='danger'/>
+			<p>Waking vehicle... This may take a while</p>
+		</span>
+	)
+	if (!vehicle) return (
+    <span>
+      <p>Nothing to see here</p>
+      {/* <Image src='/awkward.gif' alt='awkward' width='100%' height='30vh' layout='responsive' /> */}
+    </span>
+  )
+	return (
+		<Container fluid>
+			<p className={styles.description}>
+				{ stateIconMap[vehicle.state] } <b>{ vehicle.display_name }</b> - { vehicle.state }
+	    </p>
+			{vehicle.charge_state.charging_state == 'Charging' ?
+        <Charging chargeState={vehicle.charge_state} /> :
+        <Row>
+          <Col className='lg-6 g-4'>
+            <Card style={{ width: '100%' }}>
+              <Card.Body>
+                <Card.Title>🔋 Battery</Card.Title>
+                <Card.Text>{vehicle.charge_state.battery_level}% ({vehicle.charge_state.battery_range} mi)</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col className='lg-6 g-4'>
+            <Card style={{ width: '100%' }}>
+              <Card.Body>
+                <Card.Title>🔌 Charge Status</Card.Title>
+                <Card.Text>{vehicle.charge_state.charging_state}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+	      </Row>
+      }
+			<Row>
+        <Col className='lg-6 g-4'>
+          <Card style={{ width: '100%' }}>
+            <Card.Body>
+              <Card.Title>🏃 Odometer</Card.Title>
+              <Card.Text>{vehicle.vehicle_state.odometer} mi</Card.Text>
+            </Card.Body>
+					</Card>
+				</Col>
+        <Col className='lg-6 g-4'>
+          <Card style={{ width: '100%' }}>
+              <Card.Body>
+                <Card.Title>👮‍♀️ Sentry Mode</Card.Title>
+                <code>
+                {vehicle.vehicle_state.sentry_mode ?
+                'Running keep_summer_safe.exe...' :
+                'Who is Summer? / return()'
+                }
+                </code>
+              </Card.Body>
+					</Card>
+				</Col>
+			</Row>
+			<Row>
+        <Col className='lg-6 g-4'>
+          <Card style={{ width: '100%' }}>
+            <Card.Body>
+              <Card.Title>🌡 Temperature</Card.Title>
+              <Card.Text>{vehicle.climate_state.inside_temp} C | {vehicle.climate_state.outside_temp} C (out)</Card.Text>
+            </Card.Body>
+          </Card>
+				</Col>
+        <Col className='lg-6 g-4'>
+          <Card style={{ width: '100%' }}>
+            <Card.Body>
+              <Card.Title>⚙️ Software</Card.Title>
+              <Card.Text>{vehicle.vehicle_state.car_version}</Card.Text>
+            </Card.Body>
+          </Card>
+				</Col>
+			</Row>
+      <Row className='pt-5'>
+        <Col className='lg-12'>
+          <p className='text-center'>⏰ Last Updated: {new Date(vehicle.vehicle_state.timestamp).toLocaleString()}</p>
+        </Col>
+      </Row>
+    </Container>
+	)
+}
